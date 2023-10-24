b60c6aea868e8425f74025798d8a96e81cf2b282
ALogachev <aelof@yandex.ru>
2023-10-24 21:57:51 +0300

    Feature/symfony_set_up The test system controller, a dev-checks file has been created.

---
+++ src/TestingSys/Controller/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\TestingSys\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route(path: '/', methods: 'GET', name: 'app_testing_start_page')]
+    public function getStartPageAction(): Response
+    {
+        return new Response();
+    }
+}
