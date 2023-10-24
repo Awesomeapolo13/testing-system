4b4043d8e525726d9a7ccf5726ccf452fe971212
ALogachev <aelof@yandex.ru>
2023-10-24 21:52:45 +0300

    Feature/symfony_set_up The static analyser`s configs have been set up

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,26 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude(['var', 'vendor'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'cast_spaces' => [
+            'space' => 'none',
+        ],
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'types_spaces' => [
+            'space_multiple_catch' => 'single',
+        ],
+        'single_line_throw' => false,
+        'global_namespace_import' => [
+            'import_classes' => true,
+        ],
+    ])
+    ->setFinder($finder)
+;
