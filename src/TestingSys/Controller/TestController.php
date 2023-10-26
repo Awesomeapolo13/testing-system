51fb118cfb2bb38c031f3bd9de3cd9d0e6db67eb
Alexey Logachev <aleksei.logachev@coolclever.ru>
2023-10-26 17:57:31 +0300

    Feature/symfony_st_up Started to make a mock for the test page

--- src/TestingSys/Controller/TestController.php
+++ src/TestingSys/Controller/TestController.php
@@ -17,4 +17,93 @@ class TestController extends AbstractController
             'testing_sys/main.html.twig',
         );
     }
+
+    #[Route(path: '/test', methods: 'GET', name: 'app_test_page')]
+    public function getTest(): Response
+    {
+        return $this->render(
+            'testing_sys/test.html.twig',
+            [
+                'test' => [
+                    [
+                        'label' => '1 + 1 = ',
+                        'answers' => [
+                            [
+                                '3' => false,
+                            ],
+                            [
+                                '2' => false,
+                            ],
+                            [
+                                '0' => false,
+                            ],
+                        ],
+                        'correct' => null,
+                    ],
+                    [
+                        'label' => '2 + 2 = ',
+                        'answers' => [
+                            [
+                                'name' => '4',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '3 + 1',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '10',
+                                'selected' => false,
+                            ],
+                        ],
+                        'correct' => null,
+                    ],
+                    [
+                        'label' => '3 + 3 = ',
+                        'answers' => [
+                            [
+                                'name' => '1 + 5',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '1',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '6',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '2 + 4',
+                                'selected' => false,
+                            ],
+                        ],
+                        'correct' => null,
+                    ],
+                    [
+                        'label' => '4 + 4 = ',
+                        'answers' => [
+                            [
+                                'name' => '8',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '4',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '0',
+                                'selected' => false,
+                            ],
+                            [
+                                'name' => '0 + 8',
+                                'selected' => false,
+                            ],
+                        ],
+                        'correct' => null,
+                    ],
+                ],
+            ]
+        );
+    }
 }
