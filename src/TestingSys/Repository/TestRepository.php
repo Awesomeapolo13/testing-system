5ae398ea0673978ced5c97c87216ea96cd4d70bf
ALogachev <aelof@yandex.ru>
2023-10-29 14:16:24 +0300

    Feature/testing-sys The test get queries have been created

--- src/TestingSys/Repository/TestRepository.php
+++ src/TestingSys/Repository/TestRepository.php
@@ -4,6 +4,7 @@ namespace App\TestingSys\Repository;
 
 use App\TestingSys\Entity\Test;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
 
 class TestRepository extends ServiceEntityRepository
@@ -11,5 +12,36 @@ class TestRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Test::class);
+    }
+
+    public function getTestList(): array
+    {
+        return $this
+            ->createQueryBuilder('t')
+            ->select('t')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function findTesById(int $testId): ?Test
+    {
+        return $this
+            ->createQueryBuilder('t')
+            ->select([
+                't',
+                'q',
+                'a',
+            ])
+            ->leftJoin('t.questions', 'q')
+            ->leftJoin('q.answers', 'a')
+            ->where('t.id = :id')
+            ->setParameter('id', $testId)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
     }
 }
