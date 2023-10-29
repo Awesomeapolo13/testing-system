e9069f43c8bda1f56999ea20ba19a0c380af2731
ALogachev <aelof@yandex.ru>
2023-10-29 13:09:57 +0300

    Feature/symfony_set_up Added the TestGet

---
+++ tests/Unit/TestingSys/Service/TestGet.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Unit\TestingSys\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class TestGet extends TestCase
+{
+    public function test_get_questions_list(): void
+    {
+    }
+}
