85aeb1f1055b1bde12add1499c87913b7e01ac95
Alexey Logachev <aleksei.logachev@coolclever.ru>
2023-10-31 17:58:03 +0300

    Feature/testing-sys The Handle test and the get result actions have been added

--- src/TestingSys/Controller/TestController.php
+++ src/TestingSys/Controller/TestController.php
@@ -7,6 +7,7 @@ namespace App\TestingSys\Controller;
 use App\Common\Service\Serializer\Interface\NormalizerInterface;
 use App\TestingSys\Services\TestGet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +19,7 @@ class TestController extends AbstractController
     }
 
     #[Route(path: '/', methods: 'GET', name: 'app_testing_list_page')]
-    public function getStartPageAction(TestGet $testGet): Response
+    public function getTestListAction(TestGet $testGet): Response
     {
         return $this->render(
             'testing_sys/main.html.twig',
@@ -29,11 +30,29 @@ class TestController extends AbstractController
     }
 
     #[Route(path: '/test/{testId}/', methods: 'GET', name: 'app_test_page')]
-    public function getTest(int $testId, TestGet $testGet): Response
+    public function getTestDetailAction(int $testId, TestGet $testGet): Response
     {
         return $this->render(
             'testing_sys/test.html.twig',
-            ['test' => $this->normalizer->normalize($testGet->getTest($testId))]
+            [
+                'test' => $this->normalizer->normalize(
+                    $testGet->getTest($testId)
+                ),
+            ]
+        );
+    }
+
+    #[Route(path: '/test/handle/', methods: 'POST', name: 'app_test_handle_result')]
+    public function handleTestResultAction(Request $request): Response
+    {
+        return $this->redirectToRoute('app_test_get_result');
+    }
+
+    #[Route(path: '/test/result/', methods: 'GET', name: 'app_test_get_result')]
+    public function getTestResult(): Response
+    {
+        return $this->render(
+            'testing_sys/test_result.html.twig'
         );
     }
 }
